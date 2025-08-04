2f4c348a63fa5d808c34ed1f2ae478ecab50ca04
= <=>
2025-08-04 16:04:02 +0200

    Created blog post blocks with styling and animations

--- app/public/wp-content/themes/blyat-theme/index.php
+++ app/public/wp-content/themes/blyat-theme/index.php
@@ -1,10 +1,17 @@
 <?php get_header(); ?>
 
 <main>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
-    <?php endwhile; endif; ?>
+    <h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+    <section class="posts">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article class="post">
+                <h2 class="post-title"><?php the_title(); ?></h2>
+                <?php the_content(); ?>
+            </article>
+        <?php endwhile;
+        endif; ?>
+    </section>
 </main>
 
 <?php get_footer(); ?>
