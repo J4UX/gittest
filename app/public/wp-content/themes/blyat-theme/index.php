3ffc51d88c136d262221855cef15395fbecc175b
= <=>
2025-08-04 16:09:36 +0200

    Made changes to the blog post overview. Added link to single blog post page.

--- app/public/wp-content/themes/blyat-theme/index.php
+++ app/public/wp-content/themes/blyat-theme/index.php
@@ -5,10 +5,12 @@
     <p><?php bloginfo('description'); ?></p>
     <section class="posts">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <article class="post">
-                <h2 class="post-title"><?php the_title(); ?></h2>
-                <?php the_content(); ?>
-            </article>
+                <article class="post">
+                    <a href="<?php the_permalink(); ?>">
+                        <h2 class="post-title"><?php the_title(); ?></h2>
+                    </a>
+                    <?php the_excerpt(); ?>
+                </article>
         <?php endwhile;
         endif; ?>
     </section>
