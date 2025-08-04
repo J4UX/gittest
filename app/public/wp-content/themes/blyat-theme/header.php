1e6727413ef86e53553bfe8f3028d166785dcdc6
= <=>
2025-08-04 16:04:19 +0200

    Added styling imports to header

--- app/public/wp-content/themes/blyat-theme/header.php
+++ app/public/wp-content/themes/blyat-theme/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
-
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
     <?php
     // This outputs the admin bar HTML
     if (is_user_logged_in()) {
