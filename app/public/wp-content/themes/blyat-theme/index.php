3ea39b83d6b390051dbdbac540ba117fd6673070
= <=>
2025-08-04 16:28:58 +0200

    added homepage information content

--- app/public/wp-content/themes/blyat-theme/index.php
+++ app/public/wp-content/themes/blyat-theme/index.php
@@ -3,6 +3,9 @@
 <main>
     <h1><?php bloginfo('name'); ?></h1>
     <p><?php bloginfo('description'); ?></p>
+
+    <p>This is a simple blog website built with WordPress and PHP.</p>
+
     <section class="posts">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <article class="post">
