7cce4fda696657d80edf464e475d86c3f269bf4a
= <=>
2025-08-04 15:45:26 +0200

    Changes in header structure (including wp toolbar)

---
+++ app/public/wp-content/themes/blyat-theme/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+
+    <?php
+    // This outputs the admin bar HTML
+    if (is_user_logged_in()) {
+        wp_admin_bar_render();
+    }
+    ?>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="<?php echo home_url(); ?>">Home</a></li>
+            </ul>
+        </nav>
+    </header>
